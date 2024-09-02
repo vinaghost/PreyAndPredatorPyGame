f439aacec3b35078ba0f2e887a7bdd603e15da63
VINAGHOST <nguyentrungnhan87@gmail.com>
2024-09-02 14:48:04 +0700

    main

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+import sys
+from enviroment import *
+from settings import *
+from predator import *
+from prey import *
+from tree import *
+
+pygame.init()
+
+SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Earfh")
+
+clock = pygame.time.Clock()
+
+e = Enviroment(SCREEN)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or preys == [] or predators == [] or trees == []:
+            pygame.quit()
+            sys.exit()
+
+    e.update()
+
+    pygame.display.flip()
+
+    clock.tick(FPS)
